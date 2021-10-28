341fc1f87e4bdbb916dd4df971072001cdc2adc6
alisa <vne3apno@gmail.com>
2021-10-28 11:40:06 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 5_000;
+        int bonusMile = 20;
+        int totalBonus = ticketPrice / bonusMile;
+        System.out.println(totalBonus);
+
+    }
+}
